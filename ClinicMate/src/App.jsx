df614c8eefd6a5182d8ac4da374f071716152d6a
Sreeraj <psreeraj711@gmail.com>
2025-07-12 23:26:06 +0530

    app component updated

--- ClinicMate/src/App.jsx
+++ ClinicMate/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route, Navigate } from 'react-router-dom'
 import { useSelector } from 'react-redux'
 import LoginPage from './components/LoginPage'
 import CalendarView from './components/CalendarView'
+import Navbar from './components/Nabar'
 
 function App() {
   const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
@@ -9,6 +10,7 @@ function App() {
 
   return (
     <div className={isDarkMode ? 'dark' : ''}>
+      <Navbar/>
       <div className="min-h-screen bg-gray-100 dark:bg-gray-900 transition-colors">
         <div className="container mx-auto p-4">
           <Routes>
