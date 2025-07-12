fb5e584ee20fe84b4aa285525125063430d192ec
Sreeraj <psreeraj711@gmail.com>
2025-07-12 21:40:08 +0530

    store updated

--- ClinicMate/src/redux/store.js
+++ ClinicMate/src/redux/store.js
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
 import appointmentReducer from '../features/appointmentSlice'
+import authReducer from '../features/authSlice'
+import filterReducer from '../features/filterSlice'
+import darkModeReducer from '../features/darkModeSlice'
 
 export const store = configureStore({
     reducer : {
         appointments : appointmentReducer,
+        filters : filterReducer,
+        auth : authReducer,
+        darkMode : darkModeReducer,
     }
 })
